426714c0270b493043aed0e07e0c71bd63755288
yureka0920 <55544774+yureka0920@users.noreply.github.com>
2020-06-04 21:14:44 +0900

    refactoring rock,scissor,paper!
    드디어 고쳤다!

--- 실습.js
+++ 실습.js
@@ -2,58 +2,67 @@
 
 
 
- function enemy(){
-    let x = Math.random();
-    if(x<0.32) {
-        x = "scissor";
-    }
-    else if(0.32<=x<0.66){
-        x= "rock";
-    }
-    else (0.66<=x<0.99){
-        x="paper";
-    }
-};
-
-  function write(){
-    document.write('눌렀습니다.')
-};
 
 
-function battle(){
-    enemy();
-    let y = document.querySelector(this)
-    if(x=="scissor"&&y=="rock"){
-        document.write("당신은 이겼습니다!");
-    }
-    else if(x=="scissor"&&y=="scissor"){
-        document.write("당신은 비겼습니다!");
-    }
-    else if(x=="scissor"&&y=="paper"){
-        document.write("당신은 졌습니다!");
-    }
-    else if(x=="rock"&&y=="scissor"){
-        document.write("당신은 졌습니다!");
-    }
-    else if(x=="rock"&&y=="rock"){
-        document.write("당신은 비겼습니다!");
-    }
-    else if(x=="rock"&&y=="paper"){
-        document.write("당신은 이겼습니다!");
-    }
-    else if(x=="paper"&&y=="scissor"){
-        document.write("당신은 이겼습니다!");
-    }
-    else if(x=="paper"&&y=="rock"){
-        document.write("당신은 졌습니다!");
-    }
-    else(x=="paper"&&y=="paper"){
-        document.write("당신은 비겼습니다!");
-    }
-};
 
-let button = document.querySelector(button);
-button.addEventListener("click",write());
+var scissor = '가위';
+var rock = '바위';
+var paper = '보';
+
+function onbuttonClick(userInput){
+    let comInput;
+    let rnd = Math.random();
+    if(rnd<0.32) {
+          comInput= scissor;
+         
+    }
+    else if(0.32<=rnd<0.66){
+     comInput= rock;
+     
+    }
+    else {
+     comInput=paper;
+     
+    }
+
+    var result = '컴퓨터:' + comInput;
+
+    if(userInput==scissor){
+        if(comInput==rock){
+        result +=" 당신은 이겼습니다!";
+    }
+    else if(comInput==scissor){
+        result +=" 당신은 비겼습니다!";
+    }
+    else 
+    result +=" 당신은 졌습니다!";
+    
+}
+    if(userInput==rock){
+        if(comInput==scissor){
+            result +=" 당신은 졌습니다!";
+    }
+    else if(comInput==rock){
+        result +=" 당신은 비겼습니다!";
+    }
+    else 
+    result +=" 당신은 이겼습니다!";
+    }
+
+    if(userInput==paper){
+        if(comInput==scissor){
+            result +=" 당신은 이겼습니다!";
+    }
+    else if(comInput==rock){
+        result +=" 당신은 졌습니다!";
+    }
+    else
+    result +=" 당신은 비겼습니다!";
+}
+document.write(result);
+
+
+}
 
 
 
