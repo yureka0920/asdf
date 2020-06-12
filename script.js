3a2c6582207eb52a25c776ac076ff53613c4b8d0
yureka0920 <55544774+yureka0920@users.noreply.github.com>
2020-06-12 12:40:41 +0900

    API 적용

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
+
+let API_URL = 'https://floating-harbor-78336.herokuapp.com/fastfood';
+
 $(function(){
     $('.btn-search').click(function(){
-        console.log('Search!');
+        $.get(API_URL, {}, function(data){
+            console.log(data);
+        }, 'text');
     });
+});
+
+$.get(API_URL, {}, function(data){
+    let list = data.list;
+    let total = data.total;
+
+    $('.total').html('총' + total + '개의 패스트푸드점을 찾았습니다.');
+
+    let $list = $('.list');
+
+    for(let i =0; i< list.length; i++){
+        let item = list[i];
+
+        let $elem = $('#item-template')
+        .clone()
+        .removeAttr('id');
+
+        $elem.find('.item-no').html(i+1);
+        $elem.find('.item-name').html(item.name);
+        $elem.find('.item-addr').html(item.addr);
+
+        $list.append($elem)
+    }
 });
